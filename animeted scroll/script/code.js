25c5c7cd12f3d95d44cdae003e9303fdfb4d91cd
Gabriel marques do nascimento <142952581+Gabrieel-Marques-Do-Nascimento@users.noreply.github.com>
2023-09-16 15:13:38 -0300

    Update code.js

--- animeted scroll/script/code.js
+++ animeted scroll/script/code.js
@@ -20,6 +20,16 @@ const element = document.querySelectorAll('.hidden') // querySelectorAll pega a 
 // vai observar lesOne   / inpecione  isIntersecting : 'true ' = se tiver na vew port / 'False' = se tiver fora da vew port
 // myObiserver.observe(lastOne)
 
+      // run this function when the document is loaded
+      window.onload = function () {
+        // create a couple of elements in an otherwise empty HTML page
+        var heading = document.createElement("h1");
+        var heading_text = document.createTextNode("Big Head!");
+        heading.appendChild(heading_text);
+        document.body.appendChild(heading);
+      };
+ 
+
 
 
 
