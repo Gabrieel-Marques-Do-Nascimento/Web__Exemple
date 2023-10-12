477df67274455a55d1490a8f22f2affb206a8347
Gabriel marques do nascimento <142952581+Gabrieel-Marques-Do-Nascimento@users.noreply.github.com>
2023-10-12 15:28:56 -0300

    Update code.js

--- java script/functions/code.js
+++ java script/functions/code.js
@@ -1,26 +1,19 @@
-
-// anotacao simples de linha 
+// anotacao simples de linha
 /* anotacao simples de paragrafo */
 
 /** 
 * @return total sum
 function sum( ...nome)  // utilizando ...nome da variavel arry a funcao aseita quantos parametro vc quiser
-Ex : sum(1 , 2 ,3 ,4) // po diante
+Ex : sum(1 , 2 ,3 ,4) // poS diante
 */
 function sum(...args) {
-    let total = 0;
-    for (let a of args) {
-       
-        total += a;
-    }
-    return total
+  let total = 0;
+  for (let a of args) {
+    total += a;
+  }
+  return total;
 }
-console.log('TOTAL');
-
-
-
-
-
+console.log("TOTAL");
 
 // /*funcao = trcho de codego q so e executado quando chamado/invocado
 
@@ -34,7 +27,6 @@ console.log('TOTAL');
 // F8 = start
 // */
 
-
 //  /* variavel */let valor = 21
 // function incremento() {  // função void
 //  console.log("estou na função")
@@ -43,16 +35,14 @@ console.log('TOTAL');
 //  //   incremento() // chamado da função
 // //  console.log(valor)
 
-
 // // parameter e a mesma coisa q uma variavel ex: let valor = 21
 // function mostrarnome(nomedapessoa) {    //  funcao noma da função ( valor da função)
-     
+
 //     console.log(nomedapessoa) // parametro nomedapessoa so pode seer usado dentro da funcao
 // }
 // mostrarnome("gabriel")  // ("gabriel") = nomedapessoa
 // // pode se chamado varias vezes com valores diferenes
 // mostrarnome("helo whord")  // ("helo whord") = nomedapessoa
-
 
 // // função void
 // function soma(num1 , num2) { // apos virgula pode ser definido quantos parametros quiser ex; valor , valor ,......
@@ -63,13 +53,10 @@ console.log('TOTAL');
 // // valor e definido assim
 // soma(4,5)
 
-
-
-
 // function soma2 (num01 , num02) {  // funcao  return  // retortna augo para quem chama
 //     // soma os numeros e guardando na variavel
 //     /* variavel */const meusNum = num01 + num02
-   
+
 //     // retorna o numero para quem chamou
 //     return meusNum
 // }
@@ -77,10 +64,6 @@ console.log('TOTAL');
 // const numeross = soma2(5,7)
 
 // console.log(numeross)
-
-
-
-
 
 // funcao    arrow / arrow function  // seta =>
 /*
@@ -98,22 +81,3 @@ console.log(vezess)
 // funcao       parametro                  return                     numro01 X numero02         
 const Multip = ( Numero01 , Numero02 ) =>/*como se tivese um return / *  Numero01 * Numero02
     */
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
